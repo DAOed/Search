d0b3ce2d645e8c391406a4c7880162e4d8ea8a6d
dzivenu <makafuigdzivenu@gmail.com>
2019-12-05 13:38:00 +0000

    remove dummy chart data

--- src/views/Account/Activity.vue
+++ src/views/Account/Activity.vue
@@ -31,15 +31,15 @@ export default {
       details: true,
       chartOptions: {
         chart: {
-          id: "vuechart-example"
+          id: "daoed-activity"
         },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          categories: []
         }
       },
       series: [{
-        name: "series-1",
-        data: [30, 40, 35, 50, 49, 60, 70, 91]
+        name: "This year",
+        data: []
       }]
     }
   },
